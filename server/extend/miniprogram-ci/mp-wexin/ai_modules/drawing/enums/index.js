3cee9d3bc2fa66e38fe092f6bbf29aed3f07a848
陈伟基 <599263685@qq.com>
2026-01-07 18:14:01 +0800

    fix: 删除server小程序多余的打包文件

--- server/extend/miniprogram-ci/mp-wexin/ai_modules/drawing/enums/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var e=(e=>(e.PUZZLE_AI_COPYWRITER="puzzle-ai-copywriter",e.PUZZLE_COPYWRITER="puzzle-copywriter",e))(e||{});exports.ListenerTypeEnum=e;
--- server/extend/miniprogram-ci/mp-wexin/api/person_wechat.js
+++
@@ -1,1 +0,0 @@
-"use strict";const t=require("../utils/request/index.js");exports.getWeChatLists=e=>t.request.get({url:"/wechat.wechat/lists",data:e});
